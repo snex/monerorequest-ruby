bbc97740b9ac6dbbad3d47147b19dd2b79a82996
David Havlicek <snex00@protonmail.com>
2024-05-04 17:39:52 -0700

    initial commit

---
+++ lib/monerorequest/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Monerorequest
+  VERSION = "0.1.0"
+end
---
+++ spec/monerorequest_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Monerorequest do
+  it "has a version number" do
+    expect(Monerorequest::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
